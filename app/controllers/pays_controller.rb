df46a04c8652c8dc2e0ea08603bc15cf2059f04f
gototakuma <gota.main@i.softbank.jp>
2019-07-15 03:24:54 +0000

    QuickModalDone

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -1,12 +1,12 @@
 class PaysController < ApplicationController
   
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @pay = Pay.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @pay = Pay.find(params[:id])
     if @pay.update_attributes(pay_params) 
       flash[:success] = "シフト時間保存しました。"
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
-  get 'users/:user_id/pays/:date/:id/show', to: 'pays#show', as: :pays_show
-  patch 'users/:user_id/pays/:date/:id/show', to:'pays#update'
+  get 'users/:id/pays/:id/show', to: 'pays#show', as: :pays_show
+  patch 'users/:id/pays/:id/show', to:'pays#update', as: :update_pays_show
   resources :users do
     resources :pays
   end
