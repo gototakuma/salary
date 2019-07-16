2a8a6675d966546bb5366d5a41614396a7eeeec3
gototakuma <gota.main@i.softbank.jp>
2019-07-16 08:16:00 +0000

    ModalEdit

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -5,13 +5,32 @@ class PaysController < ApplicationController
     @pay = Pay.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+    @pay = Pay.find(params[:id])
+    if params[:first_day].nil?
+      @first_day = Date.today.beginning_of_month
+    else
+      @first_day = Date.parse(params[:first_day])
+    end
+    @last_day = @first_day.end_of_month
+    (@first_day..@last_day).each do |day|
+      unless @user.pays.any? {|pay| pay.worked_on == day}
+        record = @user.pays.build(worked_on: day)
+        record.save
+      end
+    end
+    @dates = @user.pays.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+  end
+  
+  
   def update
     @user = User.find(params[:id])
     @pay = Pay.find(params[:id])
     if pay_params.each do |id,item|
-      ot = Pay.find(id)
-      ot.update_attributes!(item)
-      flash[:success] = "残業申請しました"
+      pay = Pay.find(id)
+      pay.update_attributes!(item)
+      flash[:success] = "更新しました。"
       redirect_to @user
     end
     else
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
   get 'users/:id/pays/:id/new', to: 'pays#show', as: :pays_new #クイック入力モーダル
-  patch 'users/:id/pays/:id/new', to:'pays#update', as: :update_pays_new #クイック入力モーダル(update)
+  get 'users/:id/pays/:id/edit', to: 'pays#edit', as: :pays_edit#編集モーダル
+  patch 'users/:id/pays/:id/update', to:'pays#update', as: :update_pays #クイック入力モーダル(update)
   resources :users do
     resources :pays
   end
