ec244a84b16f8b6edf21b8946fa3ec1e440ef88d
gototakuma <gota.main@i.softbank.jp>
2019-07-18 08:42:48 +0000

    SumOvertime

--- app/helpers/pays_helper.rb
+++ app/helpers/pays_helper.rb
@@ -3,7 +3,15 @@ module PaysHelper
     format("%.2f",(((finished - started)/ 60) / 60))
   end
   
+  def working_times_overtime(started, finished)
+    format("%.2f",(((finished - started)/ 60) / 60) - 8.0)
+  end
+  
   def working_times_sum(seconds)
     format("%.2f", seconds / 60 / 60.0)
+  end
+  
+  def working_times_overtime_sum(secondsov)
+    format("%.2f", secondsov / 60.0 / 60.0)
   end
 end
