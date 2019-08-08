a49a794badb39a74393393eef10822ac870d6e19
gototakuma <gota.main@i.softbank.jp>
2019-08-08 04:44:46 +0000

    OverTimeDone

--- app/helpers/pays_helper.rb
+++ app/helpers/pays_helper.rb
@@ -38,4 +38,20 @@ module PaysHelper
   def nextday_overtime_pay(started,finished,basic_salary)
     (basic_salary*8)+(((((finished - started) / 60) / 60) + 16.0)*(basic_salary*1.25))
   end
+  
+  def working_times_a(started_a,started_b,finished_a,finished_b)
+    format("%.2f",(finished_a - started_a)+((finished_b - started_b) / 60.0))
+  end
+  
+  def working_overtimes(started_a,started_b,finished_a,finished_b)
+    format("%.2f",(finished_a - started_a)+((finished_b - started_b) / 60.0) - 8.0)
+  end
+  # if (Math.sign(finishb - startb) == -1) {
+  # var hour = (finisha - starta)*<%= @user.basic_salary %> - 1000;
+  # var min = Math.abs(finishb - startb)*<%= format("%.2f",(@user.basic_salary / 60.0))%> ;
+  # var result = hour + Math.floor(min);
+   
+  # var hour = (finisha - starta)*<%= @user.basic_salary %>;
+  # var min = (finishb - startb)*<%= format("%.2f",(@user.basic_salary / 60.0))%> ;
+  # var result = hour + Math.floor(min);
 end
