96e89abc54b196a1c3ab791b26547461b34cc4ec
gototakuma <gota.main@i.softbank.jp>
2019-08-20 00:13:15 +0000

    HeaderbarFixed

--- db/schema.rb
+++ db/schema.rb
@@ -45,16 +45,8 @@ ActiveRecord::Schema.define(version: 20190819085223) do
     t.integer "basic_salary"
     t.integer "basic_overtime_pay"
     t.integer "recharge"
-    t.string "provider"
-    t.string "uid"
-    t.string "image"
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.string "note"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,20 @@ User.create!(name: "後藤",
              email: "email@sample.com",
              password: "password",
              password_confirmation: "password",
+             recharge: 1030000,
+             basic_salary: 1000,
              admin: true)
 
 5.times do |n|
   name  = Faker::Name.name
   email = "email#{n+1}@sample.com"
+  recharge = 1030000
+  basic_salary = 1000
   password = "password"
   User.create!(name: name,
                email: email,
+               recharge: recharge,
+               basic_salary: basic_salary,
                password: password,
                password_confirmation: password)
 end
