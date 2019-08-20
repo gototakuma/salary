099afa128e0479d1cce1c6c32492491d64e48d1f
gototakuma <gota.main@i.softbank.jp>
2019-08-20 14:42:52 +0000

    YearResetRecharge

--- config/initializers/date_time_formats.rb
+++ config/initializers/date_time_formats.rb
@@ -1,5 +1,6 @@
 Date::DATE_FORMATS[:date] = '%-m/%d'
 Date::DATE_FORMATS[:year_month] = '%Y年%-m月'
+Date::DATE_FORMATS[:year] = '%Y'
 Date::DATE_FORMATS[:month] = '%-m月'
 Time::DATE_FORMATS[:hour] = "%H"
 Time::DATE_FORMATS[:min] = "%M"
