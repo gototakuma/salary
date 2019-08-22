e57c5e392188c047201a56a7e79e045b62a4ec5d
gototakuma <gota.main@i.softbank.jp>
2019-08-22 08:50:08 +0000

    pg5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :pays, dependent: :destroy
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
